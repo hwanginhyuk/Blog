653fa82b3cc7c4a0af4632081e0599d2a403276d
INHYUK\fit99 <fit9972@gmail.com>
2024-05-23 08:58:49 +0900

    header 추가 및 Navbar 추가

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
+// App이 BrowserRouter 안에 있어야 Link를 사용가능하다
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import AppNavbar from "../components/AppNavbar";
 
-const MyRouter = () => 
-(
+const MyRouter = () => (
     // Fragment는 생략가능하다
     <Fragment>
+        <AppNavbar/>
         <Header/>
         <h1>Hello Body</h1>
         <Footer/>
